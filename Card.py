10786d5115002615ca1a57488ca1a7a6391ab339
GGCristo <cgg.code@gmail.com>
2022-01-17 21:08:00 +0000

    Funcional, algoritmo

--- Card.py
+++ Card.py
@@ -1,19 +1,46 @@
 class Card_:
     def __init__(self, question: str, answer: str):
+        # Also known as quality of assessment.
+        # This is how difficult (as defined by the user) a flashcard is.
+        # The scale is from 0 to 5
         self.quality: int = 0
+        # This is the number of times a user sees a flashcard.
+        # 0 means they haven't studied it yet, 1 means it is their first time, and so on.
+        # It is also referred to as n in some of the documentation.
+        self.repetitions: int = 0
+        # This is also referred to as the easiness factor or EFactor or EF.
+        # It is multiplier used to increase the "space" in spaced repetition.
+        # The range is from 1.3 to 2.5
+        self.easiness: float = 2.5
+        # This is the length of time (in days) between repetitions.
+        # It is the "space" of spaced repetition
+        self.interval: int = 1
         self.question: str = question
         self.answer: str = answer
+    def updateQuality(self, quality: int):
+        assert quality >= 0 and quality <= 5
+
+        self.easiness = max(1.3, self.easiness + 0.1 - (5.0 - quality) * (0.08 + (5.0 - quality) * 0.02))
+
+        if quality < 3: self.repetitions = 0
+        else: self.repetitions += 1
+
+        if self.repetitions == 1: self.interval = 2
+        elif self.repetitions == 2: self.interval = 6
+        else: self.interval = round(self.interval * self.easiness)
 
 class cardSlice_:
     def __init__(self):
         self.slice: list[Card_] = []
         self.index: int = 0
-        print("Constructor")
     def add(self, question: str, answer: str) -> None:
         self.slice.append(Card_(question, answer))
-        sorted(self.slice, key=lambda card: card.quality)
     def get(self) -> Card_:
-        self.index += 1
-        return self.slice[self.index - 1] # TODO: que no te salga la misma pregunta seguida
+        self.slice = sorted(self.slice, key=lambda card: card.interval)
+        for i, _ in enumerate(self.slice[1:]): # Hint por si hay problemas de barajear
+            self.slice[i].interval -= 1
+        return self.slice[0]
+    def updateQuality(self, quality: int):
+        self.slice[0].updateQuality(quality)
 
 cardSlice = cardSlice_()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-import Card
+from Card import cardSlice
 from discord.ui import Button, View
 
 bot = discord.Bot()
@@ -11,40 +11,44 @@ async def on_ready():
 @bot.slash_command(guild_ids=[914264804171079702])
 async def new(ctx: discord.ApplicationContext, question: str, answer: str): # Conseguir tipo de verdad ctx
     # print(ctx.author.id)
-    Card.cardSlice.add(question, answer)
-    await ctx.respond("Carta creada")
+    cardSlice.add(question, answer)
+    await ctx.respond("Tarjeta creada")
 
 # Start session
 class AnswerButton(Button):
     quality: int
-    card: list = []
-    def __init__(self, label: int, card):
+    cardIndex: int
+    def __init__(self, label: int, ctx):
         self.quality = label
-        self.card = card
+        self.ctx = ctx
         super().__init__(label=str(label))
 
     async def callback(self, interaction: discord.Interaction):
-        # Aplicar value a question
-        # print(self.value)
+        cardSlice.updateQuality(self.quality)
         continueButton = Button(label="Continue", style=discord.ButtonStyle.green)
         closeButton =  Button(label="End Session", style=discord.ButtonStyle.red)
+        async def continueButtonCallback(interaction: discord.Interaction):
+            await interaction.response.edit_message(view=None)
+            await ask(self.ctx)
         async def closeButtonCallback(interaction: discord.Interaction):
             await interaction.response.edit_message(view=None)
+        continueButton.callback = continueButtonCallback
         closeButton.callback = closeButtonCallback
         await interaction.response.edit_message(view=View(continueButton, closeButton))
 
 @bot.slash_command(guild_ids=[914264804171079702])
 async def ask(ctx):
-    card = Card.cardSlice.get()
+    card = cardSlice.get()
     viewButtons = View()
     for i in range(6):
-        viewButtons.add_item(AnswerButton(i, card))
+        viewButtons.add_item(AnswerButton(i, ctx))
     buttonAnswer = Button(label="Show Answer")
     viewAnswer = View(buttonAnswer)
     async def buttonAnswerCallback(interaction: discord.Interaction):
-        await interaction.response.send_message(card.answer , view=viewButtons)
+        await interaction.response.edit_message(view=None)
+        await interaction.followup.send("Answer: " + card.answer , view=viewButtons)
     buttonAnswer.callback =  buttonAnswerCallback
-    await ctx.respond(card.question, view=viewAnswer)
+    await ctx.respond("Question: " + card.question, view=viewAnswer)
 
 def read_token():
     with open("token.txt", "r") as f:
